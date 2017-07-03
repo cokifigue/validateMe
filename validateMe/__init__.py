7d3019e5f41cdff03c9eebdbe26afeb7437b238e
Utkarsh Shah <utkarshshah007@gmail.com>
2017-07-03 11:11:00 -0400

    added flask-migration support

--- validateMe/__init__.py
+++ validateMe/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 from flask_restplus import Api
+from flask_migrate import Migrate
 
+import os
+
+# Initialize Flask App
 app = Flask(__name__)
-api = Api(app)
+
+# Initialize Database (and migrations)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Initialize Swagger API
+api = Api(app)
 
 import validateMe.views
